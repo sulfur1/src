fe1750ebc03026db2c8e1c5a1338a23da885dc35
sulfur1 <sulfur1917@gmail.com>
2022-04-02 20:48:56 +0300

    Create Robot.java

---
+++ algorithms/robot/Robot.java
@@ -0,0 +1,59 @@
+package algorithms.robot;
+
+class Robot {
+
+    private int x;
+    private int y;
+    private Direction dir;
+
+    public Robot(int x, int y, Direction dir) {
+        this.x = x;
+        this.y = y;
+        this.dir = dir;
+    }
+
+    void turnLeft() {
+        if      (dir == Direction.UP)    {dir = Direction.LEFT;}
+        else if (dir == Direction.DOWN)  {dir = Direction.RIGHT;}
+        else if (dir == Direction.LEFT)  {dir = Direction.DOWN;}
+        else if (dir == Direction.RIGHT) {dir = Direction.UP;}
+    }
+
+    void turnRight() {
+        if      (dir == Direction.UP)    {dir = Direction.RIGHT;}
+        else if (dir == Direction.DOWN)  {dir = Direction.LEFT;}
+        else if (dir == Direction.LEFT)  {dir = Direction.UP;}
+        else if (dir == Direction.RIGHT) {dir = Direction.DOWN;}
+    }
+
+    void stepForward() {
+        if (dir == Direction.UP)    {y++;}
+        if (dir == Direction.DOWN)  {y--;}
+        if (dir == Direction.LEFT)  {x--;}
+        if (dir == Direction.RIGHT) {x++;}
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setDirection(Direction dir) {
+        this.dir = dir;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Direction getDirection() {
+        return dir;
+    }
+}
